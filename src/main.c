c948b08a54b6c833a06790679cb20a0602006ee2
takamame0205 <40075897+takamame0205@users.noreply.github.com>
2019-11-17 15:33:22 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,11 +10,9 @@ int main( void ) {
     Lcd_Init();				// lcdを初期化する
     LCD_Clear( BLACK );		// lcdをクリアする
 
-
-
-
-    LCD_ShowString( 0, 0, "JPNHN4X / MISAKI", WHITE );
-    LCD_ShowString( 0, 0, "JPNHN4X / MISAKI", WHITE );
+    LCD_ShowString( 0, 0, "FONTX2 Header" , WHITE );
+    LCD_ShowString( 0, 20, "Height", WHITE);
+    LCD_ShowNum( 60, 20, font[1].fontheight, 2, CYAN );
 
 
 /*    LCD_ShowString( 0, 10, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", CYAN );
